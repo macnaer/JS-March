36888670060e8a4ea1e1c276d1d5e5ed7b943e6b
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-09-10 19:49:48 +0300

    API

--- 03. API/js/app.js
+++ 03. API/js/app.js
@@ -4,6 +4,16 @@ window.addEventListener("load", Init);
 function Init(){
     let url = "https://swapi.co/api/people/";
     Request(url, GetPerson);
+
+    let nextBtn = document.querySelector(".next");
+    nextBtn.addEventListener("click", Next);
+}
+
+
+function Next(){
+    console.log("Next");
+    let newUrl = state.next;
+    Request(newUrl, GetPerson);
 }
 
 function Request(url, callback) {
@@ -29,17 +39,36 @@ function Request(url, callback) {
 
 function GetPerson(persons){
     console.log(persons);
+    state.next = persons.next;
+    state.prev = persons.prev;
     let countOfPerson = document.querySelector(".count");
     countOfPerson.textContent = `10 of ${persons.count}`;
 
+    let root = document.querySelector("#root");
+    let tbody = document.createElement("tbody");
+    tbody.setAttribute("class", "table table-inbox table-hover")
+    root.appendChild(tbody);
+
+
     for (let i = 0; i < persons.results.length; i++){
-        console.log(persons.results[i].name);
-        console.log(persons.results[i].birth_year);
-        console.log(persons.results[i].gender);
-        console.log("--------------------------------");
+        let tr = document.createElement("tr");
+        let tdName = document.createElement("td");
+        tdName.textContent = persons.results[i].name;
+        let tdBirthYear = document.createElement("td");
+        tdBirthYear.textContent = persons.results[i].birth_year;
+        let tdGender = document.createElement("td");
+        tdGender.textContent = persons.results[i].gender;
+        tr.appendChild(tdName);
+        tr.appendChild(tdBirthYear);
+        tr.appendChild(tdGender);
+        tbody.appendChild(tr);
     }
 }
 
+let state = {
+    next: "",
+    prev: ""
+}
 
 
 
